bd6bf9f8f98038cb6005f43c5f2402c80f432c8f
Maiyarasu <maiyarasu@Maiyarasu.local>
2025-10-14 08:53:40 +0530

    operators added

---
+++ Operators/op.ts
@@ -0,0 +1,10 @@
+let a:number=10, b:number=11
+
+//ArithmeticOperators
+
+console.log(a+b)//additional
+console.log(b-a)//subtraction
+console.log(a*b)//multiplication
+console.log(a/b)//division
+console.log(a%b)//modular division
+console.log()
