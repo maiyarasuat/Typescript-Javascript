97abfc1f2d691045123dfcf5eeb343c3a1437f56
Maiyarasu <maiyarasu@Maiyarasu.local>
2025-10-22 13:28:07 +0530

    Do while loop

---
+++ Loops/DoWhileLoop.ts
@@ -0,0 +1,19 @@
+//DoWhile-- A do-while loop always executes atleast once  before checking the conditions
+
+/* let i:number=1
+
+do
+{
+    console.log(i)
+    i++
+}while(i<=5); */
+
+//Print value 10 to 1(descending)
+
+let i:number=10
+
+do
+{
+    console.log(i)
+    i--
+}while(i>=1)
