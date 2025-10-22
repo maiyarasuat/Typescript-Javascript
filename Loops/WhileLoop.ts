e15d1c71d2179ceb1df336527ea680dd50eaefb4
Maiyarasu <maiyarasu@Maiyarasu.local>
2025-10-22 13:14:00 +0530

    loops

---
+++ Loops/WhileLoop.ts
@@ -0,0 +1,79 @@
+//A while loop will execute as long as condition is true
+
+/* 
+
+while(condition){
+    statements;
+}
+
+*/
+
+/*
+//print the number 1 to 10
+let i:number = 1
+while(i<6){
+    console.log(i)
+    i++
+}
+*/
+
+//print the even number 1 to 10
+//method1
+
+/* let i:number=0
+
+while(i<=10){
+    console.log(i)
+    i+=2    //i=i+2;
+} */
+
+
+//method2
+/* let i:number=1
+
+while(i<=10)
+{
+    if(i%2==0){
+    console.log(i)
+    }
+    i++
+} */
+
+    //Print odd numbers from 1 to 10
+//Example1
+
+/* let i:number = 1
+while(i<=10)
+{
+    console.log(i)
+    i+=2
+} */
+
+    //Example2
+/* 
+let i:number=1
+
+while(i<=10)
+{
+    if(i%2!=0){
+        console.log(i)
+    }
+    i++
+} */
+
+//print numbers descending order 10 to 1
+/* 
+let i:number= 10
+
+while(i>=1)
+{
+    console.log(i)
+    i--
+} */
+
+    //infinite loop
+/* while(true)
+{
+    console.log(1)
+}
+ */
