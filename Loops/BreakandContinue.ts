823ed4abe2b7c2d3d6596b47bf3458aaea8284ca
Maiyarasu <maiyarasu@Maiyarasu.local>
2025-10-30 10:31:11 +0530

    loops ended

--- Loops/BreakandContinue.ts
+++ Loops/BreakandContinue.ts
@@ -1,4 +1,6 @@
-//
+//continue -->is also a jumping/skipping statement
+//break--> whenever you want to exit the for loop or terminate the loop
+//based on condition
 
 for(let i=1; i<=10; i++)
 {
@@ -18,3 +20,14 @@ for(let i=1; i<=10; i++)
     }
    console.log(i)
 }
+
+console.log("***********")
+
+for(let i=1; i<=10; i++)
+{
+    if(i==3 || i==5 || i==7){
+        continue
+    }
+    console.log(i)
+}
+
