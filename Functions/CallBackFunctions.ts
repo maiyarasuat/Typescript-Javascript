8798fb2d4451bee10f6511468706c38e7d1c386f
Maiyarasu <maiyarasu@Maiyarasu.local>
2025-11-01 11:29:22 +0530

    callback updated

--- Functions/CallBackFunctions.ts
+++ Functions/CallBackFunctions.ts
@@ -1,16 +1,45 @@
 //callback function- A function passed as an argument to another function and executed later
 
-function greet(name:string, callback:(message:string)=>void)
+
+//example1
+/*
+function greet(name:string, callback:(message:string)=> void)
 {
     console.log(name)
-    callback("Hello")
+    callback("Welcome to TypeScript")
 }
 
-
-//callback function
 function showMessage(message:string)
 {
     console.log(message)
 }
 
-greet("John", showMessage);
+greet("John" , showMessage);
+ */
+
+//example2
+
+function sum(a:number, b:number, callback:(result:number)=> void)
+{
+    let result = a+b
+    callback(result)
+}
+
+
+//callback
+function displayResult(result:number)
+{
+    console.log(result)
+}
+
+sum(12,23, displayResult)
+
+
+
+
+
+
+
+
+
+
